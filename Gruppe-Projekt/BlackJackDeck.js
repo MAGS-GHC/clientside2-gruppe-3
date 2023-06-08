bd1e4c96e89f94607a85fe8cbc4a8c055cb8077d
DNot3 <Kjetil_t@hotmail.com>
2023-06-08 14:06:07 +0200

    d

--- Gruppe-Projekt/BlackJackDeck.js
+++ Gruppe-Projekt/BlackJackDeck.js
@@ -4,12 +4,22 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
     .catch((error) => console.log("error, error"));
 
 
-/* fetch('https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=3')
-    .then((response) => response.text())
-    .then((result2) => console.log(result))
-    .catch((error) => console.log("error", error));
-console.log(result2); */
+
 function drawCard(result) {
-    console.log(result)
+    console.log(result.deck_id)
+    fetch(`https://deckofcardsapi.com/api/deck/${result.deck_id}/draw/?count=2`)
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}   
+
+function shuffleRemaining() {
+    fetch(`https://deckofcardsapi.com/api/deck/<<deck_id>>/shuffle/?remaining=true`)
+    .then((response) => response.json())
+    .then((result2) => console.log(result2))
+    .catch((error) => console.log("error", error));
+
+
 }
+
 
