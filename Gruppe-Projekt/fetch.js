ae5d7090e1a184b50e0d290b8dfb22ae2d228dc3
Datateknik-saa <127837218+Datateknik-saa@users.noreply.github.com>
2023-06-09 10:00:26 +0200

    nothing yet

--- Gruppe-Projekt/fetch.js
+++ Gruppe-Projekt/fetch.js
@@ -1,27 +1,21 @@
 
-// const ApiFetch = () => {
-//     const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`;
-//     const fetchApi = new Promise((resolve, reject) => {
-//       fetch(url)
-//         .then((response) => response.json())
-//         .then((result) => resolve(result))
-//         .catch((error) => {
-//           console.log(error);
-//           reject(error);
-//         });
-//     });
-//     fetchApi.then((apiData) => {
-//         console.log("hej")
-//         apiData = data.map( (card) => card.suit);
-//     });
-// }
-fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`)
-    .then((response) => response.json())
-    .then((result) => {
 
-        console.log(result)
-        let result1 = result.map( (card) => card.suit);
-        console.log(result1)
-    })
-    .catch((error) => console.log("error", error));
-
+const ApiFetch = () => {
+    const url = `https://deckofcardsapi.com/api/deck/fpt57qkqg4c8/draw/?count=1`;
+    const fetchApi = new Promise((resolve, reject) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((result) => resolve(result))
+        .catch((error) => {
+          console.log(error);
+          reject(error);
+        });
+    });
+    fetchApi.then((apiData) => {
+        console.log(apiData)
+        console.log(apiData.cards)
+        let deck = apiData.cards.map( (card) => card);
+        console.log(deck)
+    });
+}
+ApiFetch()
