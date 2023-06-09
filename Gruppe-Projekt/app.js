578f4d1c23726b53160a6341fa4381d2ac7dd582
Datateknik-saa <127837218+Datateknik-saa@users.noreply.github.com>
2023-06-09 12:22:34 +0200

    Udkast til OOP struktur (code)

--- Gruppe-Projekt/app.js
+++ Gruppe-Projekt/app.js
@@ -1,14 +1,18 @@
 
 class Deck {
-    constructor() {
-
+    constructor(cards) {
+        this.cards = cards;
     }  
 }
+
 class Shoe {
-    constructor() {
-        this.deck = new Deck();
+    constructor(deck) {
+        this.deck = deck;
     }  
     dealTwoCards(){
+
+    }
+    dealTwoCardsOneFrontDown(){
 
     }
     dealOneCards(){
@@ -17,11 +21,12 @@ class Shoe {
 }
 
 class BasePlayer {
-    constructor(id, name, cards, balance) {
+    constructor(id, username, email, wallet, cards) {
         this.id = id;
-        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.wallet = wallet;
         this.cards = cards;
-        this.balance = balance;
     }
     hit() {
 
@@ -35,17 +40,17 @@ class BasePlayer {
 }
 
 class Player extends BasePlayer {
-    constructor(id, name, cards, balance, currentCard) {
-        super(id, name, cards, balance);
+    constructor(id, username, email, wallet, cards, currentCard) {
+        super(id, username, email, wallet, cards);
         this.currentCard = null;
     }
     placeBet() {
 
     }
 }
-class BotPlayer extends BasePlayer {
-    constructor(id, name, deck, balance, currentCard) {
-        super(id, name, deck, balance);
+class Dealer extends BasePlayer {
+    constructor(id, username, email, wallet, cards, currentCard) {
+        super(id, username, email, wallet, cards);
         this.currentCard = null;
     }
     payOutBet() {
@@ -60,7 +65,7 @@ class Game {
     constructor() {
         this.shoe = new Shoe();
         this.player = new Player();
-        this.botPlayer = new BotPlayer();
+        this.botPlayer = new Dealer();
     }  
     start() {
 
@@ -75,4 +80,39 @@ class Game {
 
     }
 }
+let newDeck = {};
+const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`;
+const fetchApi = new Promise((resolve, reject) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => resolve(result))
+    .catch((error) => {
+      console.log(error);
+      reject(error);
+    });
+});
+fetchApi.then((apiData) => {
+const allCards = () => {
+    const url = `https://deckofcardsapi.com/api/deck/${apiData.deck_id}/draw/?count=320`;
+    const fetchApi = new Promise((resolve, reject) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((result) => resolve(result))
+        .catch((error) => {
+          console.log(error);
+          reject(error);
+        });
+    });
+    fetchApi.then((apiData) => {
+        let deck = apiData.cards.map( (card) => card);
+        console.log(deck)
+        //let newDeck = {};
+        for(let i = 0; i < deck.length; i++){
+            newDeck = new Deck(deck); 
+        }
+        console.log(newDeck)
+    });
+}
+allCards()
+});
 
--- Gruppe-Projekt/fetch.js
+++ Gruppe-Projekt/fetch.js
@@ -1,21 +1,32 @@
 
 
-const ApiFetch = () => {
-    const url = `https://deckofcardsapi.com/api/deck/fpt57qkqg4c8/draw/?count=1`;
-    const fetchApi = new Promise((resolve, reject) => {
-      fetch(url)
-        .then((response) => response.json())
-        .then((result) => resolve(result))
-        .catch((error) => {
-          console.log(error);
-          reject(error);
-        });
-    });
-    fetchApi.then((apiData) => {
-        console.log(apiData)
-        console.log(apiData.cards)
-        let deck = apiData.cards.map( (card) => card);
-        console.log(deck)
-    });
-}
-ApiFetch()
+// const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`;
+// const fetchApi = new Promise((resolve, reject) => {
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((result) => resolve(result))
+//     .catch((error) => {
+//       console.log(error);
+//       reject(error);
+//     });
+// });
+// fetchApi.then((apiData) => {
+// const allCards = () => {
+//     const url = `https://deckofcardsapi.com/api/deck/${apiData.deck_id}/draw/?count=320`;
+//     const fetchApi = new Promise((resolve, reject) => {
+//       fetch(url)
+//         .then((response) => response.json())
+//         .then((result) => resolve(result))
+//         .catch((error) => {
+//           console.log(error);
+//           reject(error);
+//         });
+//     });
+//     fetchApi.then((apiData) => {
+//         let deck = apiData.cards.map( (card) => card);
+//         console.log(deck)
+//     });
+// }
+// allCards()
+// });
+
