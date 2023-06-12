a25988bc5ebe7128eb1eddd9416ca11fd8a5ef48
Your Name <you@example.com>
2023-06-12 10:45:58 +0200

    UI startet

--- Gruppe-Projekt/js/game.js
+++
@@ -1,8 +0,0 @@
-const username = localStorage.getItem('username');
-
-if (username) {
-  console.log('Username:', username);
-  // Fetch the user's data using the username if needed
-} else {
-  // Handle the case where the username is not available in localStorage
-}
